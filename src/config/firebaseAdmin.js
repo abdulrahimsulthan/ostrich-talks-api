b82ba22f458ed7996377da193d6f35baaab249b8
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-03-16 02:10:42 +0530

    🐞 fix(typo): path
    Fixed the path of the firebase-service-account.json

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin')
 
-const serviceAccount = require('./firebase-service-account.json')
+const serviceAccount = require('../firebase-service-account.json')
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
