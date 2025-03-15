ef2ef0251761adce96ed5467e3b45e10edddf3fc
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-03-16 02:36:19 +0530

    ✨ feat(setup): MongoDB
    Connect Mongodb server URI using mongoose.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 require("dotenv").config()
 const express = require("express");
 const sendNotification = require("./controllers/sendNotification");
+const mongoose = require("mongoose")
 
 const app = express();
+
+const { PORT=5000, MONGO_URI } = process.env;
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 app.use(express.json());
 
 app.post('/send-notification', sendNotification);
 
-app.listen(process.env.PORT, () => {
-    console.log("Server running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${ PORT }`);
 });
