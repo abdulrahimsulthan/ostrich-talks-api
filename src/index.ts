1f3dc9d2911f9e248e61826f40db98c1f4a27c02
abdulrahimsulthan <rahimcsestudies@gmail.com>
2025-03-17 02:18:02 +0530

    🐞 fix(setup): path conflict
    + Created index.ts file
    + Setup rootDir and outDir in tsconfig.json

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 8000
+
+app.get("/ping", (_req, res) => {
+    res.json({message: "Hello from server."})
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}.`))
